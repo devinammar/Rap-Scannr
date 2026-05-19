4471206ffe76538d940c9d43fa019827dd179709
Devin <devinammar88@gmail.com>
2026-05-19 07:40:04 +0000

    Topbar component (Progress)

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 
 export const Topbar = () => {
+
+    const listtopbar="hover:text-white hover:scale-[1.03] transition-transform duration-200 ease"
+
     return (
         <div>
         <ul id="containertopbar" className="flex justify-center t-0 list-none text-p bg-grey/40 font-medium gap-40 pt-4 pb-4">
-            <li className="listtopbar hover:text-white hover:scale-[1.03] transition-transform duration-200 ease">
+            <li className={listtopbar}>
                 <Link to="/">Home</Link>
             </li>
-            <li className="listtopbar hover:text-white hover:scale-[1.03] transition-transform duration-200 ease">
+            <li className={listtopbar}>
                 <Link to="/search">Track Analysis</Link>
             </li>
-            <li className="listtopbar hover:text-white hover:scale-[1.03] transition-transform duration-200 ease">
+            <li className={listtopbar}>
                 <Link to="/searchbyimage">Rap Battle</Link>
             </li>
-            <li className="listtopbar hover:text-white hover:scale-[1.03] transition-transform duration-200 ease">
+            <li className={listtopbar}>
                 <Link to="/about">Profile</Link>
             </li>
         </ul>
