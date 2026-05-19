9eca2d29eb275c24aa08615e72f00b5be13c2761
Devin <devinammar88@gmail.com>
2026-05-19 08:08:48 +0000

    Added area1 component

--- src/components/home/area1.jsx
+++ src/components/home/area1.jsx
@@ -1,9 +1,19 @@
-import { Topbar } from '../topbar';
+import { Link } from "react-router-dom";
 
 export const Area1 = () => {
-    return (
-        <div>
-            <Topbar />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 className="text-h1 font-medium">Rap Scannr</h1>
+      <p className="text-p font-black">
+        Explore rap flow, lyrical cadence, and vocal delivery through
+        interactive track analysis, flow visualization, and peak speed tracking
+      </p>
+      <button>
+        <Link to="login">Login</Link>
+      </button>
+      <button>
+        <Link to="register">Register</Link>
+      </button>
+    </div>
+  );
+};
