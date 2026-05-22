f634f79534e67044bdc4a949aa6dc89685be9df7
Devin <devinammar88@gmail.com>
2026-05-22 04:34:03 +0000

    Sidebar component (Done)

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const listsidebar =
-    "";
+  const listsidebarlink =
+    "text-(--color-black) text-(--text-p) font-semibold no-underline";
 
   return (
     <>
@@ -12,27 +12,59 @@ export const Sidebar = () => {
         ☰
       </button> */}
 
-      <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
-        <img src="/hamburgericon2.png" alt="menu" />
+      <button
+        className="hidden fixed top-3 left-6 bg-transparent cursor-pointer z-[1000] border-0 max-md:block"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <img className="w-6 h-6" src="/hamburgericon-green.png" alt="menu" />
       </button>
 
       {isOpen && (
-        <div className="overlaysidebar" onClick={() => setIsOpen(false)} />
+        <div
+          className="hidden fixed top-0 left-0 w-screen h-screen bg-(--color-black) opacity-50 z-[998] max-md:block"
+          onClick={() => setIsOpen(false)}
+        />
       )}
 
-      <div className={`fixed top-0 h-screen w-[240px] bg-white z-[999] px-6 pt-[60px] pb-6 transition-all duration-300 ease-in-out ${isOpen ? "left-0" : "-left-full"}`}>
-        <ul className="containersidebar">
+      <div
+        className={`border-r-[3px] border-(--color-black) fixed top-0 h-screen w-[240px] bg-(--color-white) z-[999] px-6 pt-[60px] pb-6 transition-all duration-300 ease-in-out max-md:block ${isOpen ? "left-0" : "-left-full"}`}
+      >
+        <ul className="list-none flex flex-col gap-4 p-0">
           <li className="listsidebar">
-            <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+            <Link
+              className={listsidebarlink}
+              to="/"
+              onClick={() => setIsOpen(false)}
+            >
+              Home
+            </Link>
           </li>
           <li className="listsidebar">
-            <Link to="/trackanalysis" onClick={() => setIsOpen(false)}>Track Analysis</Link>
+            <Link
+              className={listsidebarlink}
+              to="/trackanalysis"
+              onClick={() => setIsOpen(false)}
+            >
+              Track Analysis
+            </Link>
           </li>
           <li className="listsidebar">
-            <Link to="/rapbattle" onClick={() => setIsOpen(false)}>Rap Battle</Link>
+            <Link
+              className={listsidebarlink}
+              to="/rapbattle"
+              onClick={() => setIsOpen(false)}
+            >
+              Rap Battle
+            </Link>
           </li>
           <li className="listsidebar">
-            <Link to="/profile" onClick={() => setIsOpen(false)}>Profile</Link>
+            <Link
+              className={listsidebarlink}
+              to="/profile"
+              onClick={() => setIsOpen(false)}
+            >
+              Profile
+            </Link>
           </li>
         </ul>
       </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import { Topbar } from '../components/topbar'
+import { Sidebar } from '../components/sidebar'
 
 export const Home = () => {
     return (
         <div>
             <Topbar />
+            <Sidebar />
         </div>
     )
 }
