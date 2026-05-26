6fcbdf70143e15aa6c493ab52b30afb65e5ef577
Devin <devinammar88@gmail.com>
2026-05-26 07:04:05 +0000

    Area1 component (Done)

--- src/components/home/area1.jsx
+++ src/components/home/area1.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Area1 = () => {
 
   const buttonp =
-  "text-p text-white font-medium self-center ml-6 max-[375px]:ml-0"
+  "text-p text-white font-medium self-center ml-6 max-[480px]:ml-0"
 
   return (
     <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center">
@@ -15,11 +15,11 @@ export const Area1 = () => {
             Explore rap flow, lyrical cadence, and vocal delivery through
             interactive track analysis, flow visualization, and peak speed tracking
           </p>
-          <div className="[grid-area:button] flex gap-10 justify-self-end max-[480px]:justify-self-center max-[480px]:gap-8">
-            <Link className="bg-black h-10 flex w-[222px] hover:bg-black/50 max-[768px]:w-[180px] max-[640px]:w-[160px] max-[480px]:w-[148px] max-[375px]:w-[112px] max-[375px]:justify-center" to="login">
+          <div className="[grid-area:button] flex gap-10 justify-self-end max-[480px]:justify-self-center max-[480px]:gap-8 max-[480px]:w-[calc(100vw-64px)]">
+            <Link className="bg-black h-10 flex w-[222px] hover:bg-black/50 max-[768px]:w-[180px] max-[640px]:w-[160px] max-[480px]:w-full max-[480px]:justify-center" to="login">
               <p className={buttonp}>Login</p>
             </Link>
-            <Link className="bg-grey h-10 flex w-[222px] hover:bg-grey/50 group max-[768px]:w-[180px] max-[640px]:w-[160px] max-[480px]:w-[148px] max-[375px]:w-[112px] max-[375px]:justify-center" to="register">
+            <Link className="bg-grey h-10 flex w-[222px] hover:bg-grey/50 group max-[768px]:w-[180px] max-[640px]:w-[160px] max-[480px]:w-full max-[480px]:justify-center" to="register">
               <p className={`${buttonp} group-hover:text-black`}>Register</p>
             </Link>
           </div>
