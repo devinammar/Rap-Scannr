1805c36c6ab9624c152b5db7c9ad527b8892bbbc
Devin <devinammar88@gmail.com>
2026-05-31 11:52:26 +0000

    Added area4 component

---
+++ src/components/home/area4.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export const Area4 = () => {
+    return (
+        <div>
+            <h2>Our Features</h2>
+            <Link>
+            <h3>Track Analysis</h3>
+            </Link>
+            <Link>
+            <h3>Rap Battle</h3>
+            </Link>
+            <p></p>
+        </div>
+    )
+}
