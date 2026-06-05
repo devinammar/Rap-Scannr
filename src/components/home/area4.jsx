89e0904bbc63c7f0b021cb41317cddcfbb8b005b
Devin <devinammar88@gmail.com>
2026-06-05 15:34:03 +0000

    Area4 component (Progress - The right and left distance has not been given (80px) and is not yet responsive)

--- src/components/home/area4.jsx
+++ src/components/home/area4.jsx
@@ -4,7 +4,7 @@ export const Area4 = () => {
   return (
     <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center grid justify-items-end mt-[248px]">
         <h2 className="text-h2 text-black font-medium pb-7">Our Features</h2>
-        <div className="flex flex-wrap items-end w-full justify-between">
+        <div className="flex flex-wrap items-end">
         <Link className="order-2 pr-15">
           <div className="relative w-90 h-[560px] bg-cover bg-bottom"
             style={{
@@ -23,7 +23,7 @@ export const Area4 = () => {
             <h3 className="text-h3 text-white font-medium pl-8 pb-14 absolute bottom-0">Rap Battle</h3>
           </div>
         </Link>
-        <p className="max-w-[420px] order-1 mr-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+        <p className="max-w-[580px] order-1 mr-auto pr-19">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
       </div>
     </div>
   );
