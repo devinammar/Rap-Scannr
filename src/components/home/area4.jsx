76bc6aa721cace6bfc01a4b8917e8cd91fb62778
Devin <devinammar88@gmail.com>
2026-06-07 13:39:57 +0000

    Area4 component (Progress - Not responsive yet)

--- src/components/home/area4.jsx
+++ src/components/home/area4.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export const Area4 = () => {
   return (
-    <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center grid justify-items-end mt-[248px] px-20 max-[768px]:px-14 max-[480px]:px-8 max-[1192px]:w-fit max-[1192px]:justify-self-end">
+    <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center grid justify-items-end mt-[248px] px-20 max-[768px]:px-14 max-[480px]:px-8 max-[1192px]:w-fit max-[1192px]:justify-self-end max-[940px]:w-full max-[940px]:justify-self-stretch">
         <h2 className="text-h2 text-black font-medium pb-7">Our Features</h2>
-        <div className="flex flex-nowrap items-end max-[1192px]:flex-wrap">
-        <Link className="order-2 pr-15 max-[1192px]:order-1">
-          <div className="relative w-90 h-[560px] bg-cover bg-bottom"
+        <div className="flex flex-nowrap items-end max-[1192px]:flex-wrap max-[940px]:w-full">
+        <Link className="order-2 pr-15 max-[1192px]:order-1 max-[940px]:w-full max-[940px]:pr-0 max-[940px]:pb-16">
+          <div className="relative w-90 max-[940px]:w-full h-[560px] max-[940px]:h-[400px] bg-cover bg-bottom"
             style={{
               backgroundImage: "url('/pexels-zachtheshoota-2067689.jpg')",
             }}
@@ -14,8 +14,8 @@ export const Area4 = () => {
             <h3 className="text-h3 text-white font-medium pl-8 pb-14 absolute bottom-0">Track Analysis</h3>
           </div>
         </Link>
-        <Link className="order-3 max-[1192px]:order-2">
-          <div className="relative w-90 h-[560px] bg-cover bg-center"
+        <Link className="order-3 max-[1192px]:order-2 max-[940px]:w-full">
+          <div className="relative w-90 max-[940px]:w-full h-[560px] max-[940px]:h-[400px] bg-cover bg-center"
             style={{
               backgroundImage: "url('/pexels-din-127253222-14853507.jpg')",
             }}
