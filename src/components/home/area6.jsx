82fcc64ed035683096e7e56af4b4112cad5d9a4f
Devin <devinammar88@gmail.com>
2026-06-17 06:05:25 +0000

    Area6 component (Progress)

--- src/components/home/area6.jsx
+++ src/components/home/area6.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 
 export const Area6 = () => {
+    const buttonsendmassage = "text-p text-white font-medium self-center ml-6 max-[480px]:ml-0";
+    const buttoncall = "text-p text-black font-medium self-center ml-6 max-[480px]:ml-0";
+    const sublist = "text-p text-white";
+
   return (
     <div>
       <div id="imageleft">
@@ -8,89 +12,89 @@ export const Area6 = () => {
           <img src="" />
         </Link>
       </div>
-      <div>
-        <div id="list">
+      <div className="bg-grey">
+        <div id="list" className="flex gap-18">
           <div className="listarea1">
-            <h2 className="h2">Company</h2>
-            <ul>
-              <li className="listabout">
+            <h3 className="text-h3 font-medium text-white pb-13">Company</h3>
+            <ul className="flex flex-col gap-2">
+              <li className={sublist}>
                 <Link to="/">About Us</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Partners</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">News</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Shop</Link>
               </li>
             </ul>
           </div>
           <div className="listarea2">
-            <h2 className="h2">Contact</h2>
-            <ul>
-              <li className="listabout">
+            <h3 className="text-h3 font-medium text-white pb-13">Contact</h3>
+            <ul className="flex flex-col gap-2">
+              <li className={sublist}>
                 <Link to="/">Github</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Email</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">LinkedIn</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Instagram</Link>
               </li>
             </ul>
           </div>
           <div className="listarea3">
-            <h2 className="h2">Legal & Policy</h2>
-            <ul>
-              <li className="listabout">
+            <h3 className="text-h3 font-medium text-white pb-13">Legal & Policy</h3>
+            <ul className="flex flex-col gap-2">
+              <li className={sublist}>
                 <Link to="/">Privacy & Policy</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Terms of Service</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Cookie Policy</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Lisence</Link>
               </li>
             </ul>
           </div>
           <div className="listarea4">
-            <h2 className="h2">About</h2>
-            <ul>
-              <li className="listabout">
+            <h3 className="text-h3 font-medium text-white pb-13">About</h3>
+            <ul className="flex flex-col gap-2">
+              <li className={sublist}>
                 <Link to="/">About Rap Scannr</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Features</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">How it Works</Link>
               </li>
-              <li className="listabout">
+              <li className={sublist}>
                 <Link to="/">Tech Stack</Link>
               </li>
             </ul>
           </div>
         </div>
-        <div id="2button">
+        <div id="2button" className="flex gap-10 max-[480px]:gap-8">
           <Link
-            className="bg-black h-10 flex w-[222px] hover:bg-black/50 max-[768px]:w-[180px] max-[640px]:w-[160px] max-[480px]:w-full max-[480px]:justify-center"
+            className="bg-black h-10 flex w-[222px] hover:bg-white group max-[768px]:w-[180px] max-[640px]:w-[160px] max-[480px]:w-full max-[480px]:justify-center"
             to=""
           >
-            <p className={buttonmassage}>Send Massage</p>
+            <p className={`${buttonsendmassage} group-hover:text-black`}>Send Massage</p>
           </Link>
           <Link
-            className="bg-grey h-10 flex w-[222px] hover:bg-grey/50 group max-[768px]:w-[180px] max-[640px]:w-[160px] max-[480px]:w-full max-[480px]:justify-center"
+            className="bg-white h-10 flex w-[222px] hover:bg-black group max-[768px]:w-[180px] max-[640px]:w-[160px] max-[480px]:w-full max-[480px]:justify-center"
             to=""
           >
-            <p className={`${buttoncall} group-hover:text-black`}>Call Now</p>
+            <p className={`${buttoncall} group-hover:text-white`}>Call Now</p>
           </Link>
         </div>
       </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,6 +6,7 @@ import { Area2 } from '../components/home/area2'
 import { Area3 } from '../components/home/area3'
 import { Area4 } from '../components/home/area4'
 import { Area5 } from '../components/home/area5'
+import { Area6 } from '../components/home/area6'
 
 export const Home = () => {
     const hidden = useHideOnScroll();
@@ -18,6 +19,7 @@ export const Home = () => {
             <Area3 />
             <Area4 />
             <Area5 />
+            <Area6 />
         </div>
     )
 }
