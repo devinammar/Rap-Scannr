fa520b99b48cefb15282aa98cafc4c1249318609
Devin <devinammar88@gmail.com>
2026-06-20 13:45:46 +0000

    Area1 component at track analysis page (Progress - <p> width has not been fixed and the topbar problem (topbar is not visible on a white background) has not been fixed)

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -2,10 +2,10 @@ export const Searchbar = ({ showButton = true }) => {
   return (
     <div
       id="searchbarcontainer"
-      className="min-[1920px]:w-[calc(1920px-30vw)] w-[calc(100vw-30vw)] relative flex justify-center mx-auto"
-    >
+      className="min-[1920px]:w-[1344px] w-[calc(100vw-30vw)] relative flex justify-center mx-auto"
+    > {/* min-[1920px]:w-[calc(1920px-30vw)] */}
       <input
-        className="w-full h-13 shadow-[inset_0_0_0_4px_#1D1D1D] outline-none pl-8 text-black placeholder:text-grey"
+        className="w-full h-13 bg-white shadow-[inset_0_0_0_4px_#1D1D1D] outline-none pl-8 text-black placeholder:text-grey"
         type="text"
         placeholder="Paste the music link to this bar..."
       />
--- src/components/trackanalysis/area1.jsx
+++ src/components/trackanalysis/area1.jsx
@@ -1,17 +1,57 @@
+// import { Staticbanner } from '../staticbanner'
+// import { Searchbar } from '../searchbar'
+
+// export const Area1 = () => {
+//     return (
+//         <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center mt-[5vh] relative w-full max-w-[1920px] mx-auto">
+//             <Staticbanner className="relative"/>
+//             <div className="z-10 absolute left-1/2 -translate-x-1/2 top-48 w-full max-w-[1920px] flex flex-col items-center">
+//                 <h2 className="flex justify-center text-h2 max-[640px]:text-h2-2 max-[320px]:text-h2-3 text-black font-medium pb-11">
+//                     Track Analysis
+//                 </h2>
+//                 <Searchbar showButton={true} />
+//                 <p className="text-p text-black pt-9 w-[45%] min-[1920px]:w-[864px]">
+//                     Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking
+//                 </p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// ___________________________________________________________
+
+// ini bikinan sendiri juga tapi diedit-edit lagi
+
 import { Staticbanner } from '../staticbanner'
 import { Searchbar } from '../searchbar'
 
 export const Area1 = () => {
     return (
-        <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center">
-            <h2 className="flex justify-center text-h2 max-[640px]:text-h2-2 max-[320px]:text-h2-3 text-black font-medium">Track Analysis</h2>
-            <Staticbanner />
+        <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center mt-[10vh]">
+            <Staticbanner className="relative"/>
+            <div className="z-10 absolute justify-self-center top-48 w-full max-w-[1920px]">
+            <h2 className="flex justify-center text-h2 max-[640px]:text-h2-2 max-[320px]:text-h2-3 text-white font-medium pb-11">Track Analysis</h2>
             <Searchbar showButton={true} />
-            <p className="text-p text-black pl-[15vw] pt-9 min-[1920px]:w-[calc(1920px-30vw)] w-[calc(100vw-30vw)]">Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking</p> {/* pl-[15vw] */}
-            <Staticbanner />
-            <Staticbanner />
-            <Staticbanner />
-            <Staticbanner />
+            <p className="text-p text-black pt-9 pl-[15vw] min-[1920px]:pl-[288px] min-[1920px]:w-[960px] max-[1920px]:w-[calc(100vw-50vw)]">Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking</p> {/* pl-[15vw] */}
+            </div>
         </div>
     )
 }
+
+// ___________________________________________________________
+
+// ini bikinan sendiri (default)
+
+// import { Staticbanner } from '../staticbanner'
+// import { Searchbar } from '../searchbar'
+
+// export const Area1 = () => {
+//     return (
+//         <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center mt-[5vh]">
+//             <h2 className="flex justify-center text-h2 max-[640px]:text-h2-2 max-[320px]:text-h2-3 text-black font-medium">Track Analysis</h2>
+//             <Staticbanner />
+//             <Searchbar showButton={true} />
+//             <p className="text-p text-black pl-[15vw] pt-9 min-[1920px]:w-[calc(1920px-30vw)] w-[calc(100vw-30vw)]">Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking</p> {/* pl-[15vw] */}
+//         </div>
+//     )
+// }
