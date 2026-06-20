c5db96446ec346417620dac1d411617daaf6f854
Devin <devinammar88@gmail.com>
2026-06-20 10:00:16 +0000

    Searchbar component (Progress - Searchbar width is not correct yet (for responsineness))

--- src/components/staticbanner.jsx
+++ src/components/staticbanner.jsx
@@ -1,6 +1,6 @@
 export const Staticbanner = () => {
   return (
-    <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center h-[372px]">
+    <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center h-[372px] max-[1280px]:h-[320px] max-[1024px]:h-[290px] max-[1080px]:h-[260px] max-[768px]:h-[240px]">
       <div
         className="w-full h-full bg-cover bg-center"
         style={{ backgroundImage: "url('/pexels-zachtheshoota-2067689.jpg')" }}
