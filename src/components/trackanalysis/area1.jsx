49a06c9134f215d3a6edf4a24620c02629dce253
Devin <devinammar88@gmail.com>
2026-06-20 10:37:04 +0000

    Area1 component at track analysis page (Progress)

--- src/components/trackanalysis/area1.jsx
+++ src/components/trackanalysis/area1.jsx
@@ -7,7 +7,7 @@ export const Area1 = () => {
             <h2 className="flex justify-center text-h2 max-[640px]:text-h2-2 max-[320px]:text-h2-3 text-black font-medium">Track Analysis</h2>
             <Staticbanner />
             <Searchbar showButton={true} />
-            <p className="text-p text-black">Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking</p>
+            <p className="text-p text-black pl-[15vw] pt-9 min-[1920px]:w-[calc(1920px-30vw)] w-[calc(100vw-30vw)]">Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking</p> {/* pl-[15vw] */}
             <Staticbanner />
             <Staticbanner />
             <Staticbanner />
