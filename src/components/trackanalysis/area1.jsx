8149f0221236f9241b1741946dc15e4a21ee48f4
Devin <devinammar88@gmail.com>
2026-06-20 10:19:14 +0000

    Area1 component at track analysis page (Progress)

--- src/components/trackanalysis/area1.jsx
+++ src/components/trackanalysis/area1.jsx
@@ -4,12 +4,14 @@ import { Searchbar } from '../searchbar'
 export const Area1 = () => {
     return (
         <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center">
-            <Staticbanner />
-            <Staticbanner />
-            <Staticbanner />
-            <Staticbanner />
+            <h2 className="flex justify-center text-h2 max-[640px]:text-h2-2 max-[320px]:text-h2-3 text-black font-medium">Track Analysis</h2>
             <Staticbanner />
             <Searchbar showButton={true} />
+            <p className="text-p text-black">Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking</p>
+            <Staticbanner />
+            <Staticbanner />
+            <Staticbanner />
+            <Staticbanner />
         </div>
     )
 }
