3bcd9f479636dec145b113c7cc9a9172094809ec
Devin <devinammar88@gmail.com>
2026-07-05 14:18:00 +0000

    Modified the code in appleMusicMetadata.js to fix an issue where songs could not be analyzed from Apple Music links (Done - Rap scannr now can analyze all music from all source (youtube, spotify, apple music & deezer) in track analysis feature, Next step: must test rap battle feature)

--- backend/services/sps-service/utils/appleMusicMetadata.js
+++ backend/services/sps-service/utils/appleMusicMetadata.js
@@ -1,11 +1,24 @@
 const axios = require("axios");
 
 const getAppleMusicMetadata = async (url) => {
-  // utk extract ID dari url Apple Music
-  const trackIdMatch = url.match(/[?&]i=(\d+)/);
-  const albumIdMatch = url.match(/\/album\/[^/]+\/(\d+)/);
+  // Support berbagai format URL Apple Music:
+  // /song/title/ID
+  // /album/title/ID?i=trackID
+  // /music-video/title/ID
 
-  const id = trackIdMatch ? trackIdMatch[1] : albumIdMatch ? albumIdMatch[1] : null;
+  let id = null;
+
+  // Coba ambil track ID dari ?i= param dulu
+  const trackIdMatch = url.match(/[?&]i=(\d+)/);
+  if (trackIdMatch) {
+    id = trackIdMatch[1];
+  } else {
+    // Ambil ID dari bagian terakhir URL
+    const lastIdMatch = url.match(/\/(\d+)(?:\?|$)/);
+    if (lastIdMatch) {
+      id = lastIdMatch[1];
+    }
+  }
 
   if (!id) throw new Error("Invalid Apple Music URL");
 
@@ -21,3 +34,31 @@ const getAppleMusicMetadata = async (url) => {
 };
 
 module.exports = { getAppleMusicMetadata };
+
+// __________________________________________________________________
+
+// ini yg lama
+
+// const axios = require("axios");
+
+// const getAppleMusicMetadata = async (url) => {
+//   // utk extract ID dari url Apple Music
+//   const trackIdMatch = url.match(/[?&]i=(\d+)/);
+//   const albumIdMatch = url.match(/\/album\/[^/]+\/(\d+)/);
+
+//   const id = trackIdMatch ? trackIdMatch[1] : albumIdMatch ? albumIdMatch[1] : null;
+
+//   if (!id) throw new Error("Invalid Apple Music URL");
+
+//   const response = await axios.get(`https://itunes.apple.com/lookup?id=${id}`);
+//   const item = response.data.results?.[0];
+
+//   if (!item) throw new Error("Apple Music track not found");
+
+//   return {
+//     title: item.trackName || item.collectionName,
+//     artist: item.artistName,
+//   };
+// };
+
+// module.exports = { getAppleMusicMetadata };
