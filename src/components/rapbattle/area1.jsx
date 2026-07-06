8340e67ad28f5f3e81b5d6821ac1e6b4ff4181af
Devin <devinammar88@gmail.com>
2026-07-06 08:09:27 +0000

    Fixed styling for area1.jsx (trackanalysis) and area1.jsx (rapbattle) (Done)

--- src/components/rapbattle/area1.jsx
+++ src/components/rapbattle/area1.jsx
@@ -6,7 +6,6 @@ import { compareSongs } from "../../services/api";
 export const Area1 = () => {
   const [url1, setUrl1] = useState("");
   const [url2, setUrl2] = useState("");
-
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -15,13 +14,9 @@ export const Area1 = () => {
 
     try {
       setLoading(true);
-
       const data = await compareSongs(url1, url2);
-
       setResult(data);
-
       console.log("RESULT RAP BATTLE:", data);
-
     } catch (err) {
       console.error("ERROR:", err.message);
     } finally {
@@ -35,35 +30,28 @@ export const Area1 = () => {
         image="/pexels-din-127253222-14853507.jpg"
         className="relative"
       />
-
-      <div className="z-10 absolute justify-self-center top-48 w-full max-w-[1920px]">
-
-        <h2 className="flex justify-center text-h2 text-white font-medium pb-11">
+      <div className="z-10 absolute justify-self-center top-48 max-[1280px]:top-35 max-[1024px]:top-28 max-[640px]:top-33 max-[530px]:top-38 w-full max-w-[1920px]">
+        <h2 className="flex justify-center text-h2 max-[640px]:text-h2-2 max-[530px]:text-h2-3 text-white font-medium pb-11">
           Rap Battle
         </h2>
-
-        <Searchbar
-          value={url1}
-          onChange={setUrl1}
-          showButton={false}
-        />
-
-        <h3 className="text-h3 text-black text-center py-8">
+        <Searchbar value={url1} onChange={setUrl1} showButton={false} />
+        <h3 className="text-h3 text-black font-medium text-center py-8">
           VS
         </h3>
-
-        <Searchbar
-          value={url2}
-          onChange={setUrl2}
-          showButton={false}
-        />
-
-        <button
-          onClick={handleCompare}
-          className="bg-black h-10 flex w-[222px] mx-auto mt-6 justify-center"
-        >
-          <p className="text-p text-white">Enter</p>
-        </button>
+        <Searchbar value={url2} onChange={setUrl2} showButton={false} />
+        <div className="mt-9 flex flex-row justify-between px-[15vw] min-[1920px]:px-[288px] gap-15 max-[812px]:flex-col max-[812px]:gap-8">
+          <p className="text-p text-black min-[1920px]:w-[960px] max-[1920px]:w-[calc(100vw-50vw)] max-[1522px]:w-[calc(100vw-40vw)] max-[1188px]:w-[calc(100vw-30vw)] max-[1154px]:w-[calc(100vw-30vw)] max-[1154px]:mx-auto">
+            Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking
+          </p>
+          <button
+            onClick={handleCompare}
+            className="bg-black h-10 flex w-[222px] hover:bg-black/50 max-[812px]:w-full justify-center"
+          >
+            <p className="text-p text-white font-medium self-center">
+              Enter
+            </p>
+          </button>
+        </div>
 
         {loading && (
           <p className="text-white text-center mt-5">
@@ -76,9 +64,7 @@ export const Area1 = () => {
             <p>Battle Result Ready</p>
             <p>Average Winner: {result.comparison.averageWinner}</p>
             <p>Peak Winner: {result.comparison.peakWinner}</p>
-            <p>
-              Diff Average: {result.comparison.averageDifference}
-            </p>
+            <p>Diff Average: {result.comparison.averageDifference}</p>
           </div>
         )}
 
--- src/components/trackanalysis/area1.jsx
+++ src/components/trackanalysis/area1.jsx
@@ -13,13 +13,9 @@ export const Area1 = () => {
 
     try {
       setLoading(true);
-
       const data = await analyzeSong(url);
-
       setResult(data);
-
       console.log("RESULT TRACK ANALYSIS:", data);
-
     } catch (err) {
       console.error("ERROR:", err.message);
     } finally {
@@ -29,20 +25,23 @@ export const Area1 = () => {
 
   return (
     <div className="min-[1920px]:w-[1920px] min-[1920px]:justify-self-center mt-[10vh]">
-      <Staticbanner image="/pexels-zachtheshoota-2067689.jpg" className="relative" />
-
-      <div className="z-10 absolute justify-self-center top-48 w-full max-w-[1920px]">
-
-        <h2 className="flex justify-center text-h2 text-white font-medium pb-11">
+      <Staticbanner
+        image="/pexels-zachtheshoota-2067689.jpg"
+        className="relative"
+      />
+      <div className="z-10 absolute justify-self-center top-48 max-[1280px]:top-35 max-[1024px]:top-28 max-[640px]:top-33 max-[530px]:top-38 w-full max-w-[1920px]">
+        <h2 className="flex justify-center text-h2 max-[640px]:text-h2-2 max-[530px]:text-h2-3 text-white font-medium pb-11">
           Track Analysis
         </h2>
-
         <Searchbar
           value={url}
           onChange={setUrl}
           onSearch={handleSearch}
           showButton={true}
         />
+        <p className="text-p text-black pt-9 pl-[15vw] min-[1920px]:pl-[288px] min-[1920px]:w-[960px] max-[1920px]:w-[calc(100vw-50vw)] max-[1522px]:w-[calc(100vw-40vw)] max-[1188px]:w-[calc(100vw-30vw)] max-[1154px]:pl-0 max-[1154px]:w-[calc(100vw-30vw)] max-[1154px]:mx-auto max-[1154px]:text-center">
+          Explore rap flow, lyrical cadence, and vocal delivery through interactive track analysis, flow visualization, and peak speed tracking
+        </p>
 
         {loading && (
           <p className="text-white text-center mt-5">
@@ -58,7 +57,6 @@ export const Area1 = () => {
             <p>Peak SPS: {result.stats.peakSPS}</p>
           </div>
         )}
-
       </div>
     </div>
   );
