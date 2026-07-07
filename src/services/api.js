b31de27a0ebc00543688dba3116aaeee3149f581
Devin <devinammar88@gmail.com>
2026-07-07 07:28:04 +0000

    Modified api.js, added .env (/workspaces/Rap-Scannr/.env) & modified .gitignore (/workspaces/Rap-Scannr/.gitignore) (Done – search bar input is now successfully reaching the backend)

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,48 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   timeout: 300000,
 });
 
 export const analyzeSong = async (url) => {
-  const response = await API.post("/analyze", {
-    url,
-  });
-
+  const response = await API.post("/api/analyze", { url });
   return response.data;
 };
 
 export const compareSongs = async (url1, url2) => {
-  const response = await API.post("/compare", {
-    url1,
-    url2,
-  });
-
+  const response = await API.post("/api/compare", { url1, url2 });
   return response.data;
 };
 
 export default API;
+
+// ________________________________________________________________
+
+// ini yg lama
+
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:3000",
+//   timeout: 300000,
+// });
+
+// export const analyzeSong = async (url) => {
+//   const response = await API.post("/analyze", {
+//     url,
+//   });
+
+//   return response.data;
+// };
+
+// export const compareSongs = async (url1, url2) => {
+//   const response = await API.post("/compare", {
+//     url1,
+//     url2,
+//   });
+
+//   return response.data;
+// };
+
+// export default API;
