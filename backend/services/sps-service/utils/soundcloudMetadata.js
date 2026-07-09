d43f3814ce860931d3ac232633f5ae14cc6ea0bd
Devin <devinammar88@gmail.com>
2026-07-09 07:08:39 +0000

    Modified code at soundcloudMetadata.js (Done - Rap scannr now can analyze music from soundcloud)

--- backend/services/sps-service/utils/soundcloudMetadata.js
+++ backend/services/sps-service/utils/soundcloudMetadata.js
@@ -1,8 +1,28 @@
 const SoundCloud = require("soundcloud-scraper");
+const axios = require("axios");
+
+const cleanSoundcloudUrl = async (url) => {
+  if (url.includes("on.soundcloud.com")) {
+    try {
+      const response = await axios.get(url, {
+        maxRedirects: 5,
+        validateStatus: (status) => status < 400,
+      });
+      url = response.request.res.responseUrl || url;
+    } catch {
+    }
+  }
+
+  const urlObj = new URL(url);
+  urlObj.search = "";
+  return urlObj.toString();
+};
 
 const getSoundcloudMetadata = async (url) => {
+  const cleanUrl = await cleanSoundcloudUrl(url);
+  console.log("SOUNDCLOUD CLEANED URL:", cleanUrl);
   const client = new SoundCloud.Client();
-  const song = await client.getSongInfo(url);
+  const song = await client.getSongInfo(cleanUrl);
   return {
     title: song.title,
     artist: song.author?.name || null,
