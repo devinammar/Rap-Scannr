ff4df083369ddf240b24597da7a6d9a2848f4a92
Devin <devinammar88@gmail.com>
2026-07-14 08:30:04 +0000

    Modified code server.js in sps-service (Done)

--- backend/services/sps-service/server.js
+++ backend/services/sps-service/server.js
@@ -1,11 +1,8 @@
 // ini perubahanan sebelum deploy
 
 require("dotenv").config();
-
 const app = require("./app");
-
-const PORT = process.env.SPS_SERVICE_PORT || 4000;
-
+const PORT = process.env.PORT || process.env.SPS_SERVICE_PORT || 4000;
 app.listen(PORT, () => {
   console.log(`SPS Service running on port ${PORT}`);
 });
