8e846b11e5e8446d735aed761a0964ab8e90484d
Devin <devinammar88@gmail.com>
2026-07-14 08:37:40 +0000

    Modified code server.js in gateway (Done)

--- backend/gateway/server.js
+++ backend/gateway/server.js
@@ -1,14 +1,23 @@
 // ini perubahanan sebelum deploy
 
-require("dotenv").config({ path: "../.env" });
-
+require("dotenv").config(); // hapus path "../.env"
 const app = require("./app");
-
 const PORT = process.env.PORT || process.env.GATEWAY_PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Gateway running on port ${PORT}`);
 });
+
+// _________________________________________________________
+
+// require("dotenv").config({ path: "../.env" });
+
+// const app = require("./app");
+
+// const PORT = process.env.PORT || process.env.GATEWAY_PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Gateway running on port ${PORT}`);
+// });
 
 // _________________________________________________________
 
