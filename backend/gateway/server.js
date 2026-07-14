786a55e275ad02b08defed379bd4d1a50cea0504
Devin <devinammar88@gmail.com>
2026-07-14 06:09:16 +0000

    Modified code in server.js gateway, server.js sps-service & added nixpacks.toml (Done)

--- backend/gateway/server.js
+++ backend/gateway/server.js
@@ -1,12 +1,26 @@
+// ini perubahanan sebelum deploy
+
 require("dotenv").config({ path: "../.env" });
 
 const app = require("./app");
 
-const PORT = process.env.GATEWAY_PORT || 3000;
+const PORT = process.env.PORT || process.env.GATEWAY_PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Gateway running on port ${PORT}`);
 });
+
+// _________________________________________________________
+
+// require("dotenv").config({ path: "../.env" });
+
+// const app = require("./app");
+
+// const PORT = process.env.GATEWAY_PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Gateway running on port ${PORT}`);
+// });
 
 // __________________________________________________
 
--- backend/services/sps-service/server.js
+++ backend/services/sps-service/server.js
@@ -1,3 +1,5 @@
+// ini perubahanan sebelum deploy
+
 require("dotenv").config();
 
 const app = require("./app");
@@ -7,3 +9,15 @@ const PORT = process.env.SPS_SERVICE_PORT || 4000;
 app.listen(PORT, () => {
   console.log(`SPS Service running on port ${PORT}`);
 });
+
+// ___________________________________________________________
+
+// require("dotenv").config();
+
+// const app = require("./app");
+
+// const PORT = process.env.SPS_SERVICE_PORT || 4000;
+
+// app.listen(PORT, () => {
+//   console.log(`SPS Service running on port ${PORT}`);
+// });
